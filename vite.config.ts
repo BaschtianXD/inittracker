3ae4f9b1883f3a3ad27223f98acb8f096ab7619b
Sebastian Bauer <BaschtianXD@users.noreply.github.com>
2024-06-09 20:57:58 +0200

    Update vite.config.ts
    Fixed base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://BaschtianXD.github.io/inittracker/"
+  base: "https://inittracker.baschtianxd.com"
 })
